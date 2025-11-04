e7045ddef8c74de2f661963975e8eb7d167a1c90
unknown <leanhtuan131104@gmail.com>
2025-11-04 23:23:29 +0700

    config database and user-dto ValidationPipe

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseModule } from './config/database.config';
+import { UsersModule } from './modules/users/users.module';
 
 @Module({
-  imports: [],
+  imports: [DatabaseModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
